e9489f9a070ee17594e61fedd4c633dac26a9278
Rehan Ahmed <ahmeshaf.rehan@gmail.com>
2020-05-02 01:38:32 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
 from distutils.core import setup
 setup(name='download_pubmed',
       version='1.0',
+      install_requires=[
+          'biopython',
+            'bs4',
+            'pandas'
+      ],
       py_modules=['download_pubmed'],
       )
